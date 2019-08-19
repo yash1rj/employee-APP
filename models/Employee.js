f85b59c003a8f98891a1ab0ad2fa571f5659c7a2
yash1rj <yashrj130@gmail.com>
2019-08-20 00:52:39 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var EmployeeSchema = new mongoose.Schema({
     name: String,
-    adress: String,
+    address: String,
     position: String,
     salary: Number,
     updated_at: {
