cee5b8c695bfdc1bdd806826a9eb3ad14043bb28
yash1rj <yashrj130@gmail.com>
2019-08-20 00:52:34 +0530

    Update EmployeeController.js

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -9,7 +9,80 @@ employeeController.list = (req, res) => {
             console.log("Error: ", err);
         }
         else {
-            res.send("Employees list");
+            res.render("../views/employees/index", {employees: employees});
         }
     });
 };
+
+employeeController.show = (req, res) => {
+    Employee.findOne({_id: req.params.id}).exec((err, employee) => {
+        if(err) {
+            console.log("Error: ", err);
+        }
+        else {
+            res.render("../views/employees/show", {employee: employee});
+        }
+    });
+};
+
+employeeController.create = (req, res) => {
+    res.render("../views/employees/create");
+};
+
+employeeController.save = (req, res) => {
+    var employee = new Employee(req.body);
+    console.log(employee);
+    employee.save((err) => {
+        if(err) {
+            console.log(err);
+            res.render("../views/employees/create");
+        } 
+        else {
+            console.log("Successfully created an employee.");
+            res.redirect("/employees/show/"+employee._id);
+        }
+    });
+};
+
+employeeController.edit = (req, res) => {
+    Employee.findOne({_id: req.params.id}).exec((err, employee) => {
+        if (err) {
+            console.log("Error:", err);
+        }
+        else {
+            res.render("../views/employees/edit", {employee: employee});
+        }
+    });
+};
+
+employeeController.update = (req, res) => {
+    Employee.findByIdAndUpdate(req.params.id, 
+        { 
+            $set: { 
+                name: req.body.name, 
+                address: req.body.address, 
+                position: req.body.position, 
+                salary: req.body.salary 
+            }
+        }, { new: true }, (err, employee) => {
+            if (err) {
+                console.log(err);
+                res.render("../views/employees/edit", {employee: req.body});
+            }
+            res.redirect("/employees/show/"+employee._id);
+    });
+};
+
+employeeController.delete = (req, res) => {
+    Employee.remove({_id: req.params.id}, (err) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.log("Employee deleted!");
+            res.redirect("/employees");
+        }
+    });
+};
+
+module.exports = employeeController;
